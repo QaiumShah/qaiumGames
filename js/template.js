51a86f8c94133a0cd34a4e0c43a874a2e0da94fc
Q8Sa <qaiumshah92@gmail.com>
2017-10-23 23:06:03 +0300

    Finding the largest number in multidimensional array function added in template.js

--- js/template.js
+++ js/template.js
@@ -66,3 +66,26 @@ function titleCase(str) {
 }
 
 console.log(titleCase("I'm a little tea pot"));
+
+//Finding the largest number in multidimensional array
+
+function largestOfFour(arr) {
+
+    var finalResults = [];
+    for(var i = 0; i < arr.length; i++){
+        var largest = arr[i][0];
+        for(var j = 1; j < arr[i].length; j++){
+            if(arr[i][j] > largest){
+                largest = arr[i][j];
+            }
+        }
+
+        finalResults[i] = largest ;
+
+    }
+
+
+    return finalResults;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
